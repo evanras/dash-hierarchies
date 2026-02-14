26889e811ed554ae65a184337316783e31af7b16
evanras <evanras@umich.edu>
2026-02-14 10:10:40 -0700

    version upgrade to 0.0.7

--- src/DashHierarchies.jl
+++ src/DashHierarchies.jl
@@ -3,7 +3,7 @@ module DashHierarchies
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.6"
+const version = "0.0.7"
 
 include("jl/generictablehierarchy.jl")
 include("jl/simplehierarchy.jl")
